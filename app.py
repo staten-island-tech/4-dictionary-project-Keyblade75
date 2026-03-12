38b645e2019ed2ace98dba12afd0997ec5cbaae4
Keyblade75 <abdallahe6@nycstudents.net>
2026-03-12 09:24:30 -0400

    yes

--- app.py
+++ app.py
@@ -1,6 +1,11 @@
-{
+best_buy_items= item={     
     "name": "Samsung 55\" 4K UHD TV",
     "price": 429.99,
     "department": "Televisions",
     "description": "55-inch Ultra HD Smart TV with HDR and built-in streaming apps."
-}
+}, item={"name": "Insignia 8' 4K Ultra HD HDMI Cable",
+          "price": 30.00,
+          "department":"Televisions",
+          "description":"8' 4K HDMI cable for high quality video output."}
+for index, item in enumerate(best_buy_items):
+    print(index, ":", item["name"])
---
+++ gambling.py
@@ -0,0 +1,35 @@
+def gamble(quarters,m1,m2,m3):
+        plays=0
+        one=35-m1
+        two=100-m2
+        three=10-m3
+        stage=1
+        while quarters != 0:
+                quarters-=1
+                plays +=1
+                if stage == 1:
+                        one += 1
+                        stage ==2
+                        if one == 35:
+                                quarters += 29
+                                one == 0
+                                plays -=1
+                                
+                if stage == 2: 
+                    two += 1
+                    stage == 3
+                    if two == 100:
+                        quarters += 59
+                        two == 0
+                        plays -= 1
+                if stage == 3:
+                       three += 1 
+                       stage == 1
+                       if three == 10:
+                        quarters += 9
+                        three == -1
+                        plays -=1
+                if quarters == 0:
+                      print(plays)
+                      
+gamble(48,3,10,4)
